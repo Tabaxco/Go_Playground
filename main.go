44bb63f5615e47fa094aeae9683a24c2bacab435
Tabaxco <rodriguesgui48@gmail.com>
2026-01-30 22:51:49 -0300

    Learning Go basics

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	james "strings"
+)
+
+func main() {
+	jonas := "Labareda"
+	fmt.Println("Hello, world!")
+	fmt.Printf("%s", jonas)
+	fmt.Println(james.Split(jonas, "a"))
+
+}
---
+++ modulo4/tipos.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var jonas int = 10
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+	fmt.Printf("Type: %T - Value: %v\n", "Guilherme", "Guilherme")
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	fmt.Printf("Type: %T - Value: %v\n", "2", "2")
+	fmt.Printf("Type: %T - Value: %v\n", 3.44, 3.44)
+
+	fmt.Printf("Type: %T - Value: %v\n", jonas, jonas)
+}
+
+//string - sequencia de bytes
+//float (float64/float32)

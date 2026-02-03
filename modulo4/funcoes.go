207f30184bfb90559726c473cc41b54cd96643b7
Tabaxco <rodriguesgui48@gmail.com>
2026-02-02 22:01:39 -0300

    More functions

--- modulo4/funcoes.go
+++ modulo4/funcoes.go
@@ -3,7 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(soma(42, 12))
+	somaDosValores := soma(42, 13)
+	fmt.Println(somaDosValores)
+
+	sub := subtracao(10, 5)
+	fmt.Println(sub)
+
+	nome, sobrenome := PrintaNome("MARCOS", "PAULo")
+	fmt.Println(nome, sobrenome)
+}
+
+// função com letra minúscula é PRIVADA
+// só pode ser usada no próprio pacote
+
+// função com letra maiúscula é PÚBLICA
+// pode ser usada em outros pacotes
+// pacote.PrintaNome()
+func PrintaNome(nome string, sobrenome string) (string, string) {
+	return nome, sobrenome
+	//fmt.Printf(nome)
+}
+
+func subtracao(x int, y int) int {
+	return x - y
 }
 
 func soma(x int, y int) int {
